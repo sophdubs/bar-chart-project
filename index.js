5be2536e3ba07c654f84cfd64419afbd819185a8
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-04-16 16:38:58 -0400

    Dynamically setting the width and height of the bar chart from within the drawBarChart function using CSS variables

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const drawBarChart = function(data, options, element){
-  console.log(data);
-  console.log(options);
-  console.log(element);
+  let root = document.documentElement;
+  root.style.setProperty('--height', options.height);
+  root.style.setProperty('--width', options.width);
 };
 
 const chartData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const chartOptions = {
-  height: '500px',
-  width: '700px'
+  height: '400px',
+  width: '600px'
 };
 const chartElement = $(".bar-chart");
 
