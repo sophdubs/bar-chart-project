c784d7335cf2b8d222fc58f379cae693d9d63211
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-04-20 13:53:15 -0400

    Removed hard coded bars in html and now dynamically generating them from input data in js file

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+let root = document.documentElement;
 const drawBarChart = function(data, options, element){
-  let root = document.documentElement;
+  // Setting width and height of bar chart based on passed in options.
   root.style.setProperty('--height', options.height);
   root.style.setProperty('--width', options.width);
+
+  //Creating bars in bar chart from input data
+
+  for (let i = 0; i < data.length; i++) {
+    $('.bar-list').append(`<li>${data[i]}</li>`);
+  }
+
+  //Setting width and height of bars based on data inputs
+
 };
 
-const chartData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const chartData = [1, 2, 3, 4];
 const chartOptions = {
   height: '400px',
   width: '600px'
