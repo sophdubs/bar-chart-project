ea08446143837bbb8fbd3bac5edde204e405bc36
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-04-21 14:37:27 -0400

    Fixed the position of the bars by nesting the LIs in a div and using flexbox to position them on the bottom of their parent div

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const drawBarChart = function(data, options, element){
 
   for (let i = 0; i < data.length; i++) {
     const barHeight = (data[i] * maxHeight) / maxVal;
-    console.log(barHeight);
-    $('.bar-list').append(`<li style="height: ${barHeight}px">${data[i]}</li>`);
+    $('.bar-list').append(`<div class="barDiv"><li style="height: ${barHeight}px"><p class="value">${data[i]}</p></li></div>`);
   }
 
   // Setting width of bars based on data inputs
@@ -22,14 +21,13 @@ const drawBarChart = function(data, options, element){
   const barWidth = Math.floor(Number(options.width) / numBars) - barMargin;
   root.style.setProperty('--barWidth', `${barWidth}px`);
   root.style.setProperty('--barMargin', `${Math.floor(barMargin / 2)}px`);
-
-
 };
 
 const chartData = [1, 2, 3, 4, 5, 6, 7];
 const chartOptions = {
   height: '400',
-  width: '600'
+  width: '600',
+  valuePos: 'top'
 };
 const chartElement = $(".bar-chart");
 
