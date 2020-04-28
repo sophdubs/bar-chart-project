76cdc5423917d8c2e36d35f495fd4e722f3a2f3a
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-04-28 17:04:00 -0400

    Uncommented demo page to display on github pages

--- stackedBarChartDemo.js
+++ stackedBarChartDemo.js
@@ -1,30 +1,30 @@
 // Uncomment the following code to see an example of a stacked bar chart demo.
 // Make sure to comment this code out once you are ready to create and display your own custom bar chart.
 
-// const chartData = {
-//   'barData': [[1010, 8979, 28379], [1195, 9729, 30106], [1310, 10560, 31927], [1470, 11207, 33383], [1587, 11843, 35056], [1690, 12586, 36831], [1834, 13188, 38422], [1974, 13986, 40190], [2146, 14761, 42110], [2302, 15541, 43888]],
-//   'barLabels': [15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
-//   'legend': ['Deaths', 'Recoveries', 'Cases']
-//   'barColors': ['#f59898', '#a8f3f7', '#d1c9c9'],
-// };
+const chartData = {
+  'barData': [[1010, 8979, 28379], [1195, 9729, 30106], [1310, 10560, 31927], [1470, 11207, 33383], [1587, 11843, 35056], [1690, 12586, 36831], [1834, 13188, 38422], [1974, 13986, 40190], [2146, 14761, 42110], [2302, 15541, 43888]],
+  'barLabels': [15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+  'legend': ['Deaths', 'Recoveries', 'Cases'],
+  'barColors': ['#f59898', '#a8f3f7', '#d1c9c9']
+};
 
-// const chartOptions = {
-//   height: '400',
-//   width: '700',
-//   barSpacing: '25px',
-//   valueColour: '#000000',
-//   labelColour: '#000000',
-//   title: 'Covid-19 Progression in Canada',
-//   titleColour: '#000000',
-//   titleFontSize: '40px',
-//   subtitle: 'Ten Day Report from April 15th to April 24th 2020',
-//   subtitleColour: '#000000',
-//   subtitleFontSize: '22px',
-//   xAxis: 'Dates in April 2020',
-//   yAxis: 'Number of People',
-//   tickSpacing: '10000'
-// };
+const chartOptions = {
+  height: '400',
+  width: '700',
+  barSpacing: '25px',
+  valueColour: '#000000',
+  labelColour: '#000000',
+  title: 'Covid-19 Progression in Canada',
+  titleColour: '#000000',
+  titleFontSize: '40px',
+  subtitle: 'Ten Day Report from April 15th to April 24th 2020',
+  subtitleColour: '#000000',
+  subtitleFontSize: '22px',
+  xAxis: 'Dates in April 2020',
+  yAxis: 'Number of People',
+  tickSpacing: '10000'
+};
 
-// const chartElement = $(".bar-chart");
+const chartElement = $(".bar-chart");
 
-// drawBarChart(chartData, chartOptions, chartElement);
+drawBarChart(chartData, chartOptions, chartElement);
